3c7d573f4800471717f4f2fb7f92aa626f73e261
Kang Seokhun <kangseokhun43@gmail.com>
2024-08-02 16:22:33 +0900

    Add withdrawETHAction

---
+++ src/common/LibplanetConsole.Common/Actions/WithdrawETHAction.cs
@@ -0,0 +1,94 @@
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using Bencodex;
+using Bencodex.Types;
+using Libplanet.Action;
+using Libplanet.Action.State;
+using Libplanet.Common;
+using Libplanet.Crypto;
+using Libplanet.Types.Assets;
+
+namespace LibplanetConsole.Common.Actions;
+
+[ActionType(ActionTypeValue)]
+public sealed class WithdrawETHAction : ActionBase
+{
+    private const string ActionTypeValue = "withdraw_action";
+
+    public WithdrawETHAction(Address recipient, BigInteger amount)
+    {
+        Recipient = recipient;
+        Amount = amount;
+    }
+
+    public WithdrawETHAction()
+    {
+    }
+
+    public Address Recipient { get; set; }
+
+    public BigInteger Amount { get; set; }
+
+    protected override Dictionary OnInitialize(Dictionary values)
+    {
+        return base.OnInitialize(values)
+            .Add("recipient", Recipient.Bencoded)
+            .Add("amount", new Integer(Amount));
+    }
+
+    protected override void OnLoadPlainValue(Dictionary values)
+    {
+        base.OnLoadPlainValue(values);
+        Recipient = new Address(values["recipient"]);
+        Amount = ((Integer)values["amount"]).Value;
+    }
+
+    protected override IWorld OnExecute(IActionContext context)
+    {
+        Address withdrawAddress = GetWithdrawAddress();
+        Address nonceAddress = GetNonceAddress();
+
+        Codec codec = new Codec();
+
+        var world = context.PreviousState;
+        var account = world.GetAccount(withdrawAddress);
+
+        var nonce = (Integer)(account.GetState(nonceAddress) ?? new Integer(0));
+        nonce = nonce + 1;
+
+        var data = Dictionary.Empty
+            .Add("nonce", nonce)
+            .Add("from", context.Signer.Bencoded)
+            .Add("to", Recipient.Bencoded)
+            .Add("amount", new Integer(Amount));
+
+        var hash = HashDigest<SHA1>.DeriveFrom(codec.Encode(data));
+        data = data.Add("hash", hash.Bencoded);
+        var hashAddress = new Address(hash.ToByteArray());
+
+        account = account.SetState(nonceAddress, nonce);
+        account = account.SetState(hashAddress, data);
+        world = world.SetAccount(withdrawAddress, account);
+
+        // To do: Transfer the WETH to the withdraw address.
+
+        return world;
+    }
+
+    private Address GetWithdrawAddress()
+    {
+        HashDigest<SHA1> hash = HashDigest<SHA1>.DeriveFrom(
+            Encoding.ASCII.GetBytes("libplanet_withdraw"));
+        Address address = new Address(hash.ToByteArray());
+        return address;
+    }
+
+    private Address GetNonceAddress()
+    {
+        HashDigest<SHA1> hash = HashDigest<SHA1>.DeriveFrom(
+            Encoding.ASCII.GetBytes("libplanet_withdraw_nonce"));
+        Address address = new Address(hash.ToByteArray());
+        return address;
+    }
+}
---
+++ src/node/LibplanetConsole.Nodes/Commands/WithdrawCommand.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.Composition;
+using System.Numerics;
+using JSSoft.Commands;
+using Libplanet.Crypto;
+using LibplanetConsole.Common.Actions;
+
+namespace LibplanetConsole.Nodes.Commands;
+
+[Export(typeof(ICommand))]
+[method: ImportingConstructor]
+[CommandSummary("Adds a transaction to withdraw ETH.")]
+internal sealed class WithdrawCommand(IBlockChain blockChain) : CommandAsyncBase
+{
+    [CommandPropertyRequired]
+    public required string Recipient { get; set; }
+
+    [CommandPropertyRequired]
+    public required int Amount { get; set; }
+
+    protected override async Task OnExecuteAsync(
+        CancellationToken cancellationToken, IProgress<ProgressInfo> progress)
+    {
+        var recipient = new Address(Recipient);
+        var amount = new BigInteger(Amount);
+        var action = new WithdrawETHAction(recipient, amount);
+        await blockChain.AddTransactionAsync([action], cancellationToken);
+        await Out.WriteLineAsync($"{Recipient:S}: {Amount}");
+    }
+}
